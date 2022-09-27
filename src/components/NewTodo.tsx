d4f1bda10a7ca4ae520b47cdf17461628cf9cc17
tanhoang0710 <tanhoang0710@gmail.com>
2022-09-27 11:06:57 +0700

    refs

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const NewTodo = () => {
+	const todoTextInputRef = useRef<HTMLInputElement>(null);
+
 	const submitHandler = (e: React.FormEvent) => {
 		e.preventDefault();
+
+		const enteredText = todoTextInputRef.current!.value;
 	};
 
 	return (
 		<form onSubmit={submitHandler}>
 			<label htmlFor="text">Todo text</label>
-			<input type="text" id="text" />
+			<input type="text" id="text" ref={todoTextInputRef} />
 			<button>Add Todo</button>
 		</form>
 	);
