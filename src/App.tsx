90c049d9b1e9a3c876fcf6b0c44acc8e26c6c31e
tanhoang0710 <tanhoang0710@gmail.com>
2022-09-28 10:01:48 +0700

    removing todo

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,16 @@ function App() {
 		});
 	};
 
+	const removeTodoHandler = (todoId: string) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter((todo) => todo.id !== todoId);
+		});
+	};
+
 	return (
 		<div>
 			<NewTodo onAddTodo={addTodoHandler} />
-			<Todos items={todos} />
+			<Todos items={todos} onRemoveTodo={removeTodoHandler} />
 		</div>
 	);
 }
